acdca4bf8eaddff4c727f22795a9e65658f41ade
Aletta93 <klopperaletta@gmail.com>
2016-09-28 21:03:18 +0000

    Start working on the views

--- WineHue/Wine/views.py
+++ WineHue/Wine/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
 
-# Create your views here.
+from .models import Wine_Detail
+
+class IndexView(generic.ListView):
+	template_name ='Wine/index.html'
+	context_object_name = 'wine_list'
+
+	def get_queryset(self):
+		return Wine_Detail.objects.all()
